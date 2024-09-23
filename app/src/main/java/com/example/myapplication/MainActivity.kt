61f7812934a93791a5fe08404a8f927957dcb2ca
Stanislav.Kolontsev <stas.koloncev232@gmail.com>
2024-09-23 21:45:37 +0200

    Menu_Bar 50%

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+
+        bottomNavigationView.setOnNavigationItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.item_1 -> {
+                    // Обработка нажатия на item_1
+                    true
+                }
+                R.id.item_2 -> {
+                    // Обработка нажатия на item_2
+                    true
+                }
+                R.id.item_3 -> {
+                    // Обработка нажатия на item_3
+                    true
+                }
+                R.id.item_4 -> {
+                    // Обработка нажатия на item_4
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+}
