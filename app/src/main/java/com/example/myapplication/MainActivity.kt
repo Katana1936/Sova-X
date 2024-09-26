2be7af7fdb8bf3271194f993af9c876c65aaa8b3
Stanislav.Kolontsev <stas.koloncev232@gmail.com>
2024-09-26 22:43:47 +0200

    /

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,40 +1,19 @@
 package com.example.myapplication
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.search.SearchBar
-import com.google.android.material.search.SearchView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var searchBar: SearchBar
-    private lateinit var searchView: SearchView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         searchBar = findViewById(R.id.search_bar)
-        searchView = findViewById(R.id.search_view)
-
-        searchView.setupWithSearchBar(searchBar)
-
-        searchBar.setOnClickListener {
-            searchView.show()
-        }
-
-
-        searchView.editText.setOnEditorActionListener { _, actionId, _ ->
-            if (actionId == android.view.inputmethod.EditorInfo.IME_ACTION_SEARCH) {
-
-                searchView.hide()
-                true
-            } else {
-                false
-            }
-        }
 
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
 
@@ -84,13 +63,5 @@ class MainActivity : AppCompatActivity() {
         val badgeDrawable = bottomNavigationView.getBadge(R.id.item_3)
         badgeDrawable?.isVisible = false
         badgeDrawable?.clearNumber()
-    }
-
-    override fun onBackPressed() {
-        if (searchView.isShowing) {
-            searchView.hide()
-        } else {
-            super.onBackPressed()
-        }
     }
 }
